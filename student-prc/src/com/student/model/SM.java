07a450dce2b3315e18eb7a93ccd7d34bdcfb3db5
zhnag-wu <1364240238@qq.com>
2020-12-01 17:12:17 +0800

    提交到本次仓库

---
+++ student-prc/src/com/student/model/SM.java
@@ -0,0 +1,40 @@
+package com.student.model;
+
+public class SM {
+	private int id;
+	private string username;
+	private string password;
+	private int sex;
+	private string love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public string getUsername() {
+		return username;
+	}
+	public void setUsername(string username) {
+		this.username = username;
+	}
+	public string getPassword() {
+		return password;
+	}
+	public void setPassword(string password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public string getLove() {
+		return love;
+	}
+	public void setLove(string love) {
+		this.love = love;
+	}
+	
+}
